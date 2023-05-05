987de96b06cbb6086860b51488c71465e3991a46
Matthew Bonig <matthew.bonig@gmail.com>
2023-05-04 20:37:29 -0600

    adding aws-cdk keyword

--- .projenrc.ts
+++ .projenrc.ts
@@ -12,6 +12,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   repositoryUrl: 'git@github.com:mbonig/cdk-logical-id-mapper.git',
   releaseToNpm: true,
   npmAccess: NpmAccess.PUBLIC,
+  keywords: ['aws-cdk'],
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
